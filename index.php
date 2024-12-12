2d662a35bb7f12fc7d83cb4fdf396c0976c2992e
Louzoudou <louison.rouchon@gmail.com>
2024-12-12 16:53:04 +0100

    début index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,9 +8,27 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Vivre Saint-Fortunat</title>
 </head>
+
 <body>
 
+    <header class="">
+        <nav class="navbar position-fixed fixed-bottom w-100">
+            <div class="container justify-content-center">
+                <a class="navbar-brand text-light" href="index.php">Accueil</a>
+                <a class="navbar-brand text-light" href="">Adhésion</a>
+                <a class="navbar-brand text-light" href="">Évènements</a>
+                <a class="navbar-brand text-light" href="">Contact</a>
+            </div>
+        </nav>
+        <div class="image-fond">
+        </div>
+    </header>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<section>
+
+</section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
