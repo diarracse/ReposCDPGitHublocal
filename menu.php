6dd25fa79b5ec88009193805e24f6470d0658868
mirazafind1 <153239318+mirazafind1@users.noreply.github.com>
2024-12-13 15:01:52 +0100

    ajouter menu et footer

---
+++ menu.php
@@ -0,0 +1,14 @@
+<?php
+// détecter la page active
+$pageActive = basename($_SERVER['PHP_SELF']);
+?>
+
+<!-- menu -->
+<nav class="menu">
+    <ul>
+        <li><a href="index.php" class="<?= ($pageActive == 'index.php') ? 'active' : '' ?>">Accueil</a></li>
+        <li><a href="adhesion.php" class="<?= ($pageActive == 'adhesion.php') ? 'active' : '' ?>">Adhésion</a></li>
+        <li><a href="evenements.php" class="<?= ($pageActive == 'evenements.php') ? 'active' : '' ?>">Évènements</a></li>
+        <li><a href="contact.php" class="<?= ($pageActive == 'contact.php') ? 'active' : '' ?>">Contact</a></li>
+    </ul>
+</nav>
