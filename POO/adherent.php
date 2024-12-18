500ca1cc0d6fb68e8ebd55dc4cba9ad769db883b
DoryanMaia <doryan.maia@etu.uca.fr>
2024-12-18 17:47:10 +0100

    dossier poo ok

---
+++ POO/adherent.php
@@ -0,0 +1,32 @@
+<?php
+class Adherent {
+    private $nom;
+    private $prenom;
+    private $email;
+    private $type_adhesion;
+    private $mot_de_passe;
+
+    public function __construct($nom, $prenom, $email, $type_adhesion, $mot_de_passe) {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->email = $email;
+        $this->type_adhesion = $type_adhesion;
+        $this->mot_de_passe = password_hash($mot_de_passe, PASSWORD_DEFAULT); // Hash du mot de passe
+    }
+
+    public function save(PDO $pdo) {
+        // Préparation de la requête d'insertion
+        $sql = "INSERT INTO Adherent (nom, prenom, email, type_adhesion, mot_de_passe) 
+                VALUES (:nom, :prenom, :email, :type_adhesion, :mot_de_passe)";
+        $stmt = $pdo->prepare($sql);
+
+        // Exécution de la requête
+        $stmt->execute([
+            ':nom' => $this->nom,
+            ':prenom' => $this->prenom,
+            ':email' => $this->email,
+            ':type_adhesion' => $this->type_adhesion,
+            ':mot_de_passe' => $this->mot_de_passe
+        ]);
+    }
+}
