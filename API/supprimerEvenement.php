2f7b04033741f4663a1ae7f675f4f65d5238e11b
DoryanMaia <doryan.maia@etu.uca.fr>
2024-12-18 17:33:35 +0100

    commit modif api

--- API/supprimerEvenement.php
+++ API/supprimerEvenement.php
@@ -2,8 +2,7 @@
 include("../config/config.php");
 
 try {
-    $pdo = new PDO($dsn, $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 
     if (isset($_POST['idevenement'])) {
         $idEvenement = intval($_POST['idevenement']);
