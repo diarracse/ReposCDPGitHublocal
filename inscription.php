aa11fc0808dde0f908e47dbac64f98942d0d283e
diarracse <mamediarrac13@gmail.com>
2024-12-18 14:25:31 +0100

    rajout poo inscription

--- inscription.php
+++ inscription.php
@@ -14,36 +14,41 @@
     <main class="main-form">
         <div class="form-container">
             <h2>Inscription</h2>
-            <form>
+    
+          
+
+            <form action="POO/traitement_inscription.php" method="post">
                 <div class="form-group">
                     <label for="nom">Nom</label>
-                    <input type="text" id="nom" class="form-control" required placeholder="Votre nom">
+                    <input type="text" name="nom" id="nom" class="form-control" required placeholder="Votre nom">
                 </div>
                 <div class="form-group">
                     <label for="prenom">Prénom</label>
-                    <input type="text" id="prenom" class="form-control" required placeholder="Votre prénom">
+                    <input type="text" name="prenom" id="prenom" class="form-control" required placeholder="Votre prénom">
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="email" id="email" class="form-control" required placeholder="Votre email">
+                    <input type="email" name="email" id="email" class="form-control" required placeholder="Votre email">
+                </div>
+                <div class="form-group">
+                    <label for="type_adhesion">Type d'adhésion</label>
+                    <select name="type_adhesion" id="type_adhesion" class="form-control" required>
+                    <option value="">Type d'adhésion</option>
+                    <option value="individuel">Individuel</option>
+                        <option value="famille">Famille</option>
+                        <option value="mineur">Mineur</option>
+                    </select>
                 </div>
                 <div class="form-group">
                     <label for="password">Mot de passe</label>
-                    <input type="password" id="password" class="form-control" required placeholder="Votre mot de passe">
+                    <input type="password" name="mot_de_passe" id="password" class="form-control" required placeholder="Votre mot de passe">
                 </div>
                 <div class="form-group">
                     <label for="confirm-password">Confirmer mot de passe</label>
-                    <input type="password" id="confirm-password" class="form-control" required placeholder="Confirmer votre mot de passe">
+                    <input type="password" name="mot_de_passe_confirmation" id="confirm-password" class="form-control" required placeholder="Confirmer votre mot de passe">
                 </div>
-                <div class="form-group">
-                    <label for="type-adhesion">Type d'adhésion</label>
-                    <select id="type-adhesion" class="form-control">
-                        <option value="" disabled selected>Choisissez une option</option>
-                        <option>Option 1</option>
-                        <option>Option 2</option>
-                    </select>
-                </div>
-               <a href="preference.php"> <button type="submit" class="CTA">Envoyer</button></a>
+               
+               <button type="submit" class="CTA">Envoyer</button>
                 <p class="mt-3">Vous avez déjà un compte ? <a href="connexions.php">Se connecter</a></p>
             </form>
         </div>
